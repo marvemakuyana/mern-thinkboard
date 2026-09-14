e605b41a58f79ada9ef085a174616b1f4967fdcd
marvemakuyana <marvellousmakuyana@gmail.com>
2026-09-14 17:26:07 +0200

    backend complete - added rate limiting

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,18 +5,20 @@ import dotenv from "dotenv";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-connectDB();
 
 app.use(express.json());
-
+app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server started on PORT: ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
 });
