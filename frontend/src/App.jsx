ff1b7c5f2d76c197fa15f20e5591c4019eb41649
marvemakuyana <marvellousmakuyana@gmail.com>
2026-09-16 17:36:48 +0200

    add tailwind, daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div data-theme="cyberpunk">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
